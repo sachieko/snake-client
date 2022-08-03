d073895dca63d40d9d72e2e508fc73cf358f7ccf
sachieko <kirazetsubou@gmail.com>
2022-08-03 17:44:37 -0600

    Added auto-move and fun messages.

--- input.js
+++ input.js
@@ -1,13 +1,20 @@
 let connection;
+let int;
 const handleUserInput = (data) => {
   if (data === '\u0003') console.log("Exiting!...\n"), process.exit();
-  if (data === 'w') connection.write(up);
-  if (data === 's') connection.write(down);
-  if (data === 'a') connection.write(left);
-  if (data === 'd') connection.write(right);
+  if (data === 'w') int ? clearInterval(int) : null, int = setInterval(() => connection.write(up), 110);
+  if (data === 'a') int ? clearInterval(int) : null, int = setInterval(() => connection.write(left), 110);
+  if (data === 's') int ? clearInterval(int) : null, int = setInterval(() => connection.write(down), 110);
+  if (data === 'd') int ? clearInterval(int) : null, int = setInterval(() => connection.write(right), 110);
+  if (data === '1') int ? clearInterval(int) : null, connection.write(greet), int;
+  if (data === '2') int ? clearInterval(int) : null, connection.write(meme), int;
+  if (data === '3') int ? clearInterval(int) : null, connection.write(meme2), int;
+  if (data === '4') int ? clearInterval(int) : null, connection.write(meme3), int;
+  if (data === '5') int ? clearInterval(int) : null, connection.write(eat), int;
 };
 const up = "Move: up", left = "Move: left", right = "Move: right", down = "Move: down";
-const setupInput = function (conn) {
+const greet = "Say: Hello there", meme = "Say: General Kenobi", meme2 = "Say: You are a bold one", meme3 = "Say: AYAYA!", eat = "Say: Nom!";
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
