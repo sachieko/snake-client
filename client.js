4d104e3b7527a2237c3b027944909253ca473922
sachieko <kirazetsubou@gmail.com>
2022-08-03 16:14:12 -0600

    Added exit message

--- client.js
+++ client.js
@@ -13,16 +13,15 @@ const connect = () => {
   });
   return client;
 };
+const handleUserInput = (data) => {
+  if (data === '\u0003') console.log("Exiting!...\n"), process.exit();
+};
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", (data) => {
-    if (data === "\u0003") {
-      process.exit();
-    }
-  });
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 module.exports = {
