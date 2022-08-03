c59ea200a368024097c434eec98593d065360c6f
sachieko <kirazetsubou@gmail.com>
2022-08-03 14:06:11 -0600

    Separated into modules. Still idle-death

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require('net');
+
+const connect = () => {
+  const client = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  client.setEncoding('utf8');
+  client.on("data", (data) => {
+    console.log(data);
+  });
+
+  return client;
+};
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,26 +1,10 @@
-const net = require('net');
-
+const connect = require('./client');
 // DO NOT SHARE THIS IP.
 // const IP = '127.0.0.1';
 
 // const NAME = 'Sachieko';
-const input = require('process').stdin;
-const connect = () => {
-  const client = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-    input: input
-  });
+// const input = require('process').stdin;
 
-  client.setEncoding('utf8');
-  client.on("data", (data) => {
-    console.log(data);
-  });
-  // input.on('w', () => {
-    // client.write(`Move: Up`);
-  // });
-  return client;
-};
 
 console.log("Connecting ...");
 connect();
