8c8cbd24cf4ea66d3a1fbec9378031867ac76b7f
sachieko <kirazetsubou@gmail.com>
2022-08-03 18:22:30 -0600

    Contains constants for client

---
+++ constants.js
@@ -0,0 +1,18 @@
+const constants = {
+  IP: '165.227.47.243',
+  PORT: 50541,
+  UP: "Move: up",
+  LEFT: "Move: left",
+  RIGHT: "Move: right",
+  DOWN: "Move: down",
+  GREET: "Say: Hello there",
+  MEME: "Say: General Kenobi",
+  MEME2: "Say: You are a bold one",
+  MEME3: "Say: AYAYA!",
+  EAT: "Say: Nomnom!",
+  CONNMSG: "Connecting to snek...",
+  EXITMSG: "Exiting program!\n",
+  NAME: "Name: SSS",
+  QUITKEY: "\u0003"
+};
+module.exports = constants;
